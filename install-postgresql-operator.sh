79cb6db0539fd37962a71f10241e94c1bfd41acc
Jin Huang <huangjinzhuo@gmail.com>
2020-06-19 23:30:42 -0700

    Install Crunchy PostgreSQL

---
+++ install-postgresql-operator.sh
@@ -0,0 +1,4 @@
+#### Install PostgreSQL Operator via GCP Markeplace
+
+# Follow instruction to install Crunchy PostgreSQL for GKE here:
+# https://console.cloud.google.com/marketplace/details/crunchydata/crunchy-postgresql-operator
