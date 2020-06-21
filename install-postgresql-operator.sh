ce30a9779042c796d758f9e3265ec143b6c3a7e3
Jin Huang <huangjinzhuo@gmail.com>
2020-06-20 20:07:38 -0700

    CRDs and Pods

--- install-postgresql-operator.sh
+++ install-postgresql-operator.sh
@@ -2,3 +2,11 @@
 
 # Follow instruction to install Crunchy PostgreSQL for GKE here:
 # https://console.cloud.google.com/marketplace/details/crunchydata/crunchy-postgresql-operator
+
+# CRDs:
+# pgclusters.crunchydata.com
+# pgreplicas.crunchydata.com
+# pgpolicies.crunchydata.com
+# pgtasks.crunchydata.com
+# Pods: operator, apiserver, scheuduler, event
+# 
