3bf8cb3799c0179741f51c4fb69732c05279e7b4
Jin Huang <huangjinzhuo@gmail.com>
2020-06-20 21:42:46 -0700

    CRDs and Pods

--- install-postgresql-operator.sh
+++ install-postgresql-operator.sh
@@ -8,5 +8,13 @@
 # pgreplicas.crunchydata.com
 # pgpolicies.crunchydata.com
 # pgtasks.crunchydata.com
+
 # Pods: operator, apiserver, scheuduler, event
-# 
+#
+
+# Create a high-availability PostgreSQL cluster that has a single replica, 
+# supports having backups in both a local storage area and GCP Cloud Storage
+# (or Amazon S3) and has built-in metrics and connection pooling
+pgo create cluster hacluster --replica-count=1 --metrics \
+--pgbackrest-storage-type="local,s3" --pgbouncer --pgbadger
+
